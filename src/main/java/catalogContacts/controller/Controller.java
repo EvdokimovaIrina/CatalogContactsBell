61c10d066b682cc17a9f349b94af5831a2494811
Evdokimova Irina dom <IrenEvdokimova@gmail.com>
2017-07-26 16:47:58 +0400

    Добавлено описание в интерфейс

--- src/main/java/catalogContacts/controller/Controller.java
+++ src/main/java/catalogContacts/controller/Controller.java
@@ -9,66 +9,73 @@ import java.util.Map;
  */
 public interface Controller {
     /**
-     * Redirects to the server to add a new contact
+     * Redirects to the service to add a new contact
      *
-     * @param name
+     * @param name  Contact name entered by the user
      */
     void addContact(String name);
 
     /**
      * Redirects to the service to add a new group
      *
-     * @param name Contact name entered by the user
+     * @param name Group name entered by the user
      */
     void addGroup(String name);
 
     /**
      * Redirecting to a service to add new contactDetails to a contact
      *
-     * @param number
-     * @param mapDetails
+     * @param number Number in the list of the changed contact
+     * @param mapDetails Data on contact information in Map, contact type and value
      */
     void addDetails(int number, Map<TypeContact, String> mapDetails);
 
     /**
-     * @param numberContact
-     * @param numberGroup
+     *  Redirecting to a service to add group to an existing contact
+     * @param numberContact Number in the list of the changed contact
+     * @param numberGroup Group number in the general list to add to the contact
      */
     void addGroupToContact(int numberContact, int numberGroup);
 
     /**
-     * @param numberContact
-     * @param numberGroup
+     *Redirect to a service to delete a group from existing contact
+     * @param numberContact Group number in the general list to add to the contact
+     * @param numberGroup Group number in the general list to remove from the contact
      */
     void deleteGroupToContact(int numberContact, int numberGroup);
 
     /**
-     * @param numberGroup
+     * Redirect to the service to list the contacts
+     * @param numberGroup Group number, if you want to list the contacts of the group. If the entire list, then the null value
      */
     void showContactList(Integer numberGroup);
 
     /**
-     * @param numberContact
+     * Redirect to the service for displaying contact information
+     * @param numberContact Number of the contact whose personal information needs to be displayed
      */
     void showDetails(Integer numberContact);
 
     /**
-     *
+     *Redirect to the service to list the group
      */
     void showGroupList();
 
     /**
-     * @param numberContact
+     * Redirect to a service to remove a contact from the list
+     * @param numberContact Number in the list of the contact to be deleted
      */
     void deletContact(int numberContact);
 
     /**
-     * @param numberContact
-     * @param numberContactDetails
+     * Redirect to a service to remove a contact information
+     * @param numberContact Number in the list of the changed contact
+     * @param numberContactDetails Number in the list contact of the contactDetails to be deleted
      */
     void deletContactDetails(int numberContact, int numberContactDetails);
 
     /**
+     * Redirection to the service for the editing of contact information
      * @param numberContact
      * @param numberContactDetails
      * @param value
@@ -76,19 +83,22 @@ public interface Controller {
     void ChangeSelectedContactDetails(int numberContact, int numberContactDetails, String value);
 
     /**
-     * @param numberGroup
+     * Redirect to a service to remove a group from the list
+     * @param numberGroup Number in the list of the group to be deleted
      */
     void deletGroup(int numberGroup);
 
     /**
-     * @param numberGroup
-     * @param value
+     * Redirect to a service for editing a group
+     * @param numberGroup   Number in the list of the changed group
+     * @param value New value for the name of the group
      */
     void changeGroup(int numberGroup, String value);
 
     /**
-     * @param numberContact
-     * @param value
+     * Redirect to a service for editing a contact
+     * @param numberContact Number in the list of the changed contact
+     * @param value New value for the name of the contact
      */
     void changeContact(int numberContact, String value);
 
