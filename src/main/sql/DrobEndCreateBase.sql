b15fc68a89e7260209696fae244f7ad4c09f7342
Evdokimova Irina dom <IrenEvdokimova@gmail.com>
2017-08-08 12:47:06 +0400

    1

--- src/main/sql/DrobEndCreateBase.sql
+++ src/main/sql/DrobEndCreateBase.sql
@@ -1,19 +1,2 @@
-﻿SELECT * FROM  "TUser" ;
-SELECT * FROM  "Contact" ;
-
-SELECT InsertUser('user6','222');
-
-SELECT ChangeUser(1,'user6','222');
-
-SELECT * FROM  "Contact" ;
-
-SELECT InsertContact(1,'Иванов');
-
-
-SELECT * FROM  getListGroupInContact(2) ;
-
-SELECT * FROM  getContactDetails(1) ;
-
-SELECT DeleteContact(1);
-
-SELECT DeleteUser(1);
+﻿DROP DATABASE CatalogContacs;
+CREATE DATABASE CatalogContacs;
