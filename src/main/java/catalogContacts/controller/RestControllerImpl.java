580df2349f05879b2ebf245b2c3e59ac2874e345
Evdokimova Irina dom <IrenEvdokimova@gmail.com>
2017-09-05 01:33:30 +0400

    корректировка REST controller

--- src/main/java/catalogContacts/controller/RestControllerImpl.java
+++ src/main/java/catalogContacts/controller/RestControllerImpl.java
@@ -1,14 +1,23 @@
 package catalogContacts.controller;
 
+import catalogContacts.controller.evenResult.EventType;
 import catalogContacts.controller.evenResult.FactoryRestResult;
+import catalogContacts.controller.evenResult.RestResult;
+import catalogContacts.dao.exception.DaoException;
+import catalogContacts.model.Contact;
+import catalogContacts.model.ContactDetails;
+import catalogContacts.model.Group;
+import catalogContacts.model.TypeContact;
 import catalogContacts.service.ContactService;
 import catalogContacts.service.GroupService;
 import catalogContacts.service.UserService;
 import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -27,37 +36,31 @@ public class RestControllerImpl {
     @Resource(name = "factoryRestResult")
     private FactoryRestResult factoryRestResult;
 
-    /*@RequestMapping(value = "/contact", method = RequestMethod.POST)
-    public RestResult addContact(@RequestParam(value="userid") int userid,
-                                  @RequestParam(value="name") String contactName) {
+    @RequestMapping(value = "/contacts", method = RequestMethod.POST)
+    public RestResult addContact(@RequestParam(value="name") String contactName) {
         logger.debug("Добавление нового контакта");
         try {
-            userService.setUserThread(userid);
             return factoryRestResult.getSuccessResult(EventType.CONTACT,contactService.addContact(contactName));
         } catch (DaoException e) {
             return factoryRestResult.getFailResult(e);
         }
     }
 
-    @RequestMapping(value = "/contact", method = RequestMethod.PUT)
-    public RestResult changeContact(@RequestParam(value="userid") int userid,
-                                  @RequestParam(value="id") int id,
+    @RequestMapping(value = "/contacts/{contactId}", method = RequestMethod.PUT)
+    public RestResult changeContact(@PathVariable(value="contactId") int id,
                                   @RequestParam(value="name") String newName) {
         logger.debug("Изменение наименования контакта");
         try {
-            userService.setUserThread(userid);
             return factoryRestResult.getSuccessResult(EventType.CONTACT,contactService.changeContact(id,newName));
         } catch (DaoException e) {
             return factoryRestResult.getFailResult(e);
         }
     }
 
-    @RequestMapping(value = "/contact", method = RequestMethod.DELETE)
-    public RestResult deleteContact(@RequestParam(value="userid") int userid,
-                                  @RequestParam(value="id") int id) {
+    @RequestMapping(value = "/contacts/{contactId}", method = RequestMethod.DELETE)
+    public RestResult deleteContact(@PathVariable(value="contactId") int id) {
         logger.debug("Удаление контакта");
         try {
-            userService.setUserThread(userid);
             contactService.deleteContact(id);
             return factoryRestResult.getSuccessResult(EventType.SUCCESS,true);
         } catch (DaoException e) {
@@ -65,37 +68,57 @@ public class RestControllerImpl {
         }
     }
 
-    @RequestMapping(value = "/group", method = RequestMethod.POST)
-    public RestResult addGroup(@RequestParam(value="userid") int userid,
-                                  @RequestParam(value="name", required=true) String groupName) {
-        logger.debug("Добавление нового контакта");
+    @RequestMapping(value = "/contacts/{contactId}", method = RequestMethod.GET)
+    public RestResult showContact(@PathVariable(value="contactId")int id) {
         try {
-            userService.setUserThread(userid);
+            Contact contact = contactService.getContactByNumber(id);
+            return factoryRestResult.getSuccessResult(EventType.CONTACT,contact);
+        } catch (DaoException e) {
+            return factoryRestResult.getFailResult(e);
+        }
+    }
+
+    @RequestMapping(value = "/contacts", method = RequestMethod.GET)
+    public RestResult findByNameContact(@RequestParam(value="name",required = false) String name,
+                                        @RequestParam(value="groupid",required = false) Integer groupId) {
+        try {
+            List<Contact> contactList;
+            if (name!=null){
+                contactList = contactService.findByName(name);
+            }else{
+                contactList = contactService.showContactList(groupId);
+            }
+            return factoryRestResult.getSuccessResult(EventType.CONTACTLIST,contactList);
+        } catch (DaoException e) {
+            return factoryRestResult.getFailResult(e);
+        }
+    }
+
+    @RequestMapping(value = "/groups", method = RequestMethod.POST)
+    public RestResult addGroup(@RequestParam(value="name") String groupName) {
+        logger.debug("Добавление новой группы");
+        try {
             return factoryRestResult.getSuccessResult(EventType.GROUP,groupService.addGroup(groupName));
         } catch (DaoException e) {
             return factoryRestResult.getFailResult(e);
         }
     }
 
-    @RequestMapping(value = "/group", method = RequestMethod.PUT)
-    public RestResult changeGroup(@RequestParam(value="userid") int userid,
-                                  @RequestParam(value="id") int id,
+    @RequestMapping(value = "/groups/{groupId}", method = RequestMethod.PUT)
+    public RestResult changeGroup(@PathVariable(value="groupId") int id,
                                   @RequestParam(value="name") String newName) {
         logger.debug("Изменение наименования группы");
         try {
-            userService.setUserThread(userid);
-            return factoryRestResult.getSuccessResult(EventType.GROUP,groupService.changeGroup(id,newName));
+              return factoryRestResult.getSuccessResult(EventType.GROUP,groupService.changeGroup(id,newName));
         } catch (DaoException e) {
             return factoryRestResult.getFailResult(e);
         }
     }
 
-    @RequestMapping(value = "/group", method = RequestMethod.DELETE)
-    public RestResult deleteGroup(@RequestParam(value="userid") int userid,
-                                  @RequestParam(value="id") int id) {
+    @RequestMapping(value = "/groups/{groupId}", method = RequestMethod.DELETE)
+    public RestResult deleteGroup(@PathVariable(value="groupId") int id) {
         logger.debug("Удаление группы");
         try {
-            userService.setUserThread(userid);
             groupService.deleteGroup(id);
             return factoryRestResult.getSuccessResult(EventType.SUCCESS,true);
         } catch (DaoException e) {
@@ -103,10 +126,29 @@ public class RestControllerImpl {
         }
     }
 
-    @RequestMapping(value = "/contactDetails", method = RequestMethod.POST)
-    public RestResult addContactDetails(@RequestParam(value="contactid") int contactid,
-                               @RequestParam(value="type") String type,
-                               @RequestParam(value="value") String value) {
+    @RequestMapping(value = "/groups/{groupId}", method = RequestMethod.GET)
+    public RestResult showGroup(@PathVariable(value="groupId")int id) {
+        try {
+            Group group = groupService.findByNumber(id);
+            return factoryRestResult.getSuccessResult(EventType.GROUP,group);
+        } catch (DaoException e) {
+            return factoryRestResult.getFailResult(e);
+        }
+    }
+    @RequestMapping(value = "/groups", method = RequestMethod.GET)
+    public RestResult showGroupList() {
+        try {
+            List <Group> groupList= groupService.showGroupList();
+            return factoryRestResult.getSuccessResult(EventType.GROUPLIST,groupList);
+        } catch (DaoException e) {
+            return factoryRestResult.getFailResult(e);
+        }
+    }
+
+    @RequestMapping(value = "/contactDetailses/{contactid}", method = RequestMethod.POST)
+    public RestResult addContactDetails(@PathVariable(value="contactid") int contactid,
+                                        @RequestParam(value="type") String type,
+                                        @RequestParam(value="value") String value) {
         logger.debug("Добавление детальной информации");
         try {
             Map<TypeContact, String> mapDetails = new HashMap<>();
@@ -117,10 +159,10 @@ public class RestControllerImpl {
         }
     }
 
-    @RequestMapping(value = "/contactDetails", method = RequestMethod.PUT)
-    public RestResult ChangeContactDetails(@RequestParam(value="contactid") int contactid,
-                                  @RequestParam(value="id") int id,
-                                  @RequestParam(value="value") String value) {
+    @RequestMapping(value = "/contactDetails/{contactid}", method = RequestMethod.PUT)
+    public RestResult ChangeContactDetails(@PathVariable(value="contactid") int contactid,
+                                           @RequestParam(value="id") int id,
+                                           @RequestParam(value="value") String value) {
         logger.debug("Изменение детальной информации");
         try {
             return factoryRestResult.getSuccessResult(EventType.DETAILS,contactService.ChangeSelectedContactDetails(contactid,id,value));
@@ -128,10 +170,11 @@ public class RestControllerImpl {
             return factoryRestResult.getFailResult(e);
         }
     }
+    @ModelAttribute
 
-    @RequestMapping(value = "/contactDetails", method = RequestMethod.DELETE)
-    public RestResult addContact2(@RequestParam(value="contactid") int contactid,
-                                  @RequestParam(value="id") int id) {
+    @RequestMapping(value = "/contactDetailses/{detailsID}/contacts/{contactid}", method = RequestMethod.DELETE)
+    public RestResult deleteDetails(@PathVariable(value="contactid") int contactid,
+                                    @PathVariable(value="detailsID")int id) {
         logger.debug("Удаление детальной информации");
         try {
             contactService.deleteContactDetails(contactid,id);
@@ -141,66 +184,8 @@ public class RestControllerImpl {
         }
     }
 
-    @RequestMapping(value = "/contacts/{contactId}", method = RequestMethod.GET)
-    public RestResult showContact(@RequestParam(value="userid") int userId,
-                                  @PathVariable(value="contactId")int id) {
-        try {
-            SecurityContextHolder.setLoggedUserID(userId);
-            Contact contact = contactService.getContactByNumber(id);
-            return factoryRestResult.getSuccessResult(EventType.CONTACT,contact);
-        } catch (DaoException e) {
-            return factoryRestResult.getFailResult(e);
-        }
-    }
-
-    @RequestMapping(value = "/contacts", method = RequestMethod.GET)
-    public RestResult findByNameContact(@RequestParam(value="userid") int userId,
-                                        @RequestParam(value="name") String name) {
-        try {
-            SecurityContextHolder.setLoggedUserID(userId);
-            List<Contact> contactList = contactService.findByName(name);
-            return factoryRestResult.getSuccessResult(EventType.CONTACTLIST,contactList);
-        } catch (DaoException e) {
-            return factoryRestResult.getFailResult(e);
-        }
-    }
-
-    @RequestMapping(value = "/contacts", method = RequestMethod.GET)
-    public RestResult showContactList(@RequestParam(value="userid") int userId,
-                                      @RequestParam(value="groupid",required = false) Integer groupId) {
-        try {
-            SecurityContextHolder.setLoggedUserID(userId);
-            List <Contact> contactList = contactService.showContactList(groupId);
-            return factoryRestResult.getSuccessResult(EventType.CONTACTLIST,contactList);
-        } catch (DaoException e) {
-            return factoryRestResult.getFailResult(e);
-        }
-    }
-
-    @RequestMapping(value = "/group", method = RequestMethod.GET)
-    public RestResult showGroup(@RequestParam(value="userid") int userId,
-                                @RequestParam(value="id")int id) {
-        try {
-            SecurityContextHolder.setLoggedUserID(userId);
-            Group group = groupService.findByNumber(id);
-            return factoryRestResult.getSuccessResult(EventType.GROUP,group);
-        } catch (DaoException e) {
-            return factoryRestResult.getFailResult(e);
-        }
-    }
-    @RequestMapping(value = "/groups", method = RequestMethod.GET)
-    public RestResult showGroupList(@RequestParam(value="id")int id) {
-        try {
-            SecurityContextHolder.setLoggedUserID(id);
-            List <Group> groupList = groupService.showGroupList();
-            return factoryRestResult.getSuccessResult(EventType.GROUPLIST,groupList);
-        } catch (DaoException e) {
-            return factoryRestResult.getFailResult(e);
-        }
-    }
-
-    @RequestMapping(value = "/contactDetailses", method = RequestMethod.GET)
-    public RestResult showContactDetailses(@RequestParam(value="contactid")int contactid) {
+    @RequestMapping(value = "/contactDetailses/{contactid}", method = RequestMethod.GET)
+    public RestResult showContactDetailses(@PathVariable(value="contactid")int contactid) {
         try {
             Contact contact = contactService.getContactByNumber(contactid);
             List <ContactDetails> contactDetails = contactService.showContactDetails(contact);
@@ -209,7 +194,7 @@ public class RestControllerImpl {
             return factoryRestResult.getFailResult(e);
         }
     }
-*/
+
     public ContactService getContactService() {
         return contactService;
     }
